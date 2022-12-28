87ad24b455018b1eb3fca33831fd16b93dc2c2f4
timDeHof <ttdehof@gmail.com>
2022-12-27 22:55:56 -0500

    created a checkbox & checkgroup components (#21)

---
+++ features/ui/checkbox/checkbox.tsx
@@ -0,0 +1,222 @@
+import React, { useRef } from "react";
+import styled, { css } from "styled-components";
+import { color, textFont } from "@styles/theme";
+
+export enum CheckboxSize {
+  sm = "sm",
+  md = "md",
+}
+export enum CheckboxState {
+  Unchecked = "unchecked",
+  Checked = "checked",
+  PartlyChecked = "partlyChecked",
+}
+
+export type CheckboxProps = {
+  size: CheckboxSize;
+  state: CheckboxState;
+  disabled: boolean;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  text?: string;
+  value?: string;
+};
+
+const CheckboxContainer = styled.div<{ size: CheckboxSize }>`
+  display: flex;
+  margin: 0.3rem;
+  vertical-align: middle;
+  ${(props) => {
+    switch (props.size) {
+      case CheckboxSize.sm:
+        return css`
+          ${textFont("sm", "medium")}
+          gap: 0.5rem;
+        `;
+      case CheckboxSize.md:
+        return css`
+          ${textFont("md", "medium")}
+          gap: 0.8rem;
+        `;
+    }
+  }}
+`;
+
+const Icon = styled.svg<{ disabled: boolean }>`
+  width: 100%;
+  height: 100%;
+  fill: none;
+  stroke: ${(props) =>
+    props.disabled ? color("gray", 300) : color("primary", 700)};
+  stroke-width: "2";
+  stroke-linecap: "round";
+  stroke-linejoin: "round";
+  background-position: center;
+`;
+
+const HiddenCheckbox = styled.input.attrs({ type: "checkbox" })`
+  border: none;
+  clip: rect(0 0 0 0);
+  clip-path: inset(50%);
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: relative;
+  white-space: nowrap;
+`;
+
+const StyledCheckbox = styled.div<{
+  state: CheckboxState;
+  disabled: boolean;
+  size: CheckboxSize;
+}>`
+  display: inline-block;
+  width: ${(props) => (props.size === CheckboxSize.sm ? "1rem" : "1.3rem")};
+  height: ${(props) => (props.size === CheckboxSize.sm ? "1rem" : "1.3rem")};
+  border: 1px solid ${color("gray", 300)};
+  border-radius: ${(props) =>
+    props.size === CheckboxSize.sm ? "0.3rem" : "0.4rem"};
+  ${(props) => {
+    switch (props.state) {
+      case CheckboxState.Unchecked:
+        return css`
+          background: ${props.disabled ? color("gray", 100) : "#FFF"};
+          border-color: ${
+            props.disabled ? color("gray", 200) : color("gray", 300)
+          };
+          &:hover {
+            background: ${
+              props.disabled ? color("gray", 100) : color("primary", 50)
+            };
+            border-color: ${
+              props.disabled ? color("gray", 200) : color("primary", 600)
+            };
+              }
+          &:focus {
+            background: "#FFF";
+            outline: color("primary", 300);
+            box-shadow: 0 0 0 4px color("primary", 100);
+            }
+          }
+        `;
+      case CheckboxState.Checked:
+        return css`
+          background: ${props.disabled
+            ? color("gray", 100)
+            : color("primary", 50)};
+          border-color: ${props.disabled
+            ? color("gray", 200)
+            : color("gray", 300)};
+          &:hover {
+            background: ${props.disabled
+              ? color("gray", 100)
+              : color("primary", 50)};
+            border-color: ${props.disabled
+              ? color("gray", 200)
+              : color("primary", 600)};
+          }
+          &:focus {
+            background: "#FFF";
+            outline: color("primary", 300);
+            box-shadow: 0 0 0 4px color("primary", 100);
+          }
+        `;
+      case CheckboxState.PartlyChecked:
+        return css`
+          background: ${props.disabled ? color("gray", 100) : "#FFF"};
+          border-color: ${
+            props.disabled ? color("gray", 200) : color("gray", 300)
+          };
+          &:hover {
+            background: ${
+              props.disabled ? color("gray", 100) : color("primary", 50)
+            };
+            border-color: ${
+              props.disabled ? color("gray", 200) : color("primary", 600)
+            };
+              }
+          &:focus {
+            background: "#FFF";
+            outline: color("primary", 300);
+            box-shadow: 0 0 0 4px color("primary", 100);
+            }
+          }
+        `;
+    }
+  }}
+  transition: all 150ms;
+  cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
+
+  ${(props) =>
+    props.state === CheckboxState.PartlyChecked &&
+    css`
+      background: ${color("primary", 50)};
+      border-color: ${color("primary", 600)};
+    `}
+`;
+
+const CheckboxText = styled.div<{ size: CheckboxSize; disabled: boolean }>`
+  ${(props) => {
+    switch (props.size) {
+      case CheckboxSize.sm:
+        return css`
+          ${textFont("sm", "medium")}
+          gap: 0.5rem;
+        `;
+      case CheckboxSize.md:
+        return css`
+          ${textFont("md", "medium")}
+          gap: 0.8rem;
+        `;
+    }
+  }}
+  color: ${(props) =>
+    props.disabled ? color("gray", 300) : color("gray", 700)};
+`;
+
+export const Checkbox = ({
+  state,
+  disabled,
+  size,
+  text,
+  onChange,
+}: CheckboxProps) => {
+  const ref = useRef<HTMLInputElement>(null);
+
+  const handleChange = () => {
+    if (ref.current) {
+      ref.current.click();
+    }
+  };
+
+  return (
+    <CheckboxContainer size={size}>
+      <HiddenCheckbox
+        ref={ref}
+        checked={state === CheckboxState.Checked}
+        disabled={disabled}
+        onChange={onChange}
+      />
+      <StyledCheckbox
+        state={state}
+        disabled={disabled}
+        size={size}
+        onChange={handleChange}
+      >
+        {state === CheckboxState.PartlyChecked ? (
+          <Icon viewBox="0 0 10 2" disabled={disabled}>
+            <path d="M1.5 1H8.5" />
+          </Icon>
+        ) : (
+          state === CheckboxState.Checked && (
+            <Icon viewBox="0 0 10 8" disabled={disabled}>
+              <path d="M9 1L3.5 6.5L1 4" />
+            </Icon>
+          )
+        )}
+      </StyledCheckbox>
+      <CheckboxText size={size} disabled={disabled}>
+        {text}
+      </CheckboxText>
+    </CheckboxContainer>
+  );
+};
---
+++ features/ui/checkboxGroup/checkboxGroup.tsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import styled, { css } from "styled-components";
+import { Checkbox, CheckboxState, CheckboxSize } from "../checkbox/checkbox";
+import { textFont } from "@styles/theme";
+
+export type CheckboxGroupOptions = {
+  label: string;
+  value: string;
+  children?: CheckboxGroupOptions[];
+};
+
+export type CheckboxGroupProps = {
+  options: CheckboxGroupOptions[];
+  selectedValues: string[];
+  disabled: boolean;
+  size: CheckboxSize;
+  onChange: (selectedValues: string[]) => void;
+};
+
+const CheckboxGroupContainer = styled.div<{ size: CheckboxSize }>`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 0.5rem;
+  margin: 0.3rem;
+  vertical-align: middle;
+  ${(props) => {
+    switch (props.size) {
+      case CheckboxSize.sm:
+        return css`
+          ${textFont("sm", "medium")}
+          gap: 0.5rem;
+        `;
+      case CheckboxSize.md:
+        return css`
+          ${textFont("md", "medium")}
+          gap: 0.8rem;
+        `;
+    }
+  }}
+`;
+const CheckboxIndent = styled.div<{ size: CheckboxSize }>`
+  width: ${(props) => (props.size === CheckboxSize.sm ? "0.8rem" : "1rem")};
+  height: ${(props) => (props.size === CheckboxSize.sm ? "0.8rem" : "1rem")};
+`;
+export const CheckboxGroup: React.FC<CheckboxGroupProps> = ({
+  options,
+  selectedValues = [],
+  size = CheckboxSize.md,
+  disabled = false,
+  onChange,
+}) => {
+  const [internalSelectedValues, setInternalSelectedValues] =
+    useState(selectedValues);
+
+  const renderOptions = (options: CheckboxGroupOptions[], depth = 0) => {
+    return (
+      <ul style={{ listStyle: "none", padding: 0, margin: 0 }}>
+        {options.map((option) => {
+          const isChecked = internalSelectedValues.includes(option.value);
+          const isPartlyChecked =
+            option.children &&
+            option.children.some(
+              (childOption) =>
+                internalSelectedValues.includes(childOption.value) &&
+                !option.children?.every((childOption) =>
+                  internalSelectedValues.includes(childOption.value)
+                )
+            );
+
+          return (
+            <li key={option.value}>
+              <CheckboxGroupContainer size={size} key={option.value}>
+                {depth > 0 && <CheckboxIndent size={size} />}
+                <Checkbox
+                  size={size}
+                  state={
+                    isChecked
+                      ? CheckboxState.Checked
+                      : isPartlyChecked
+                      ? CheckboxState.PartlyChecked
+                      : CheckboxState.Unchecked
+                  }
+                  disabled={disabled}
+                  text={option.label}
+                  onChange={() => {
+                    let newSelectedValue: string[];
+                    if (isChecked) {
+                      // if the option is already checked, we remove it from the selected values
+                      newSelectedValue = internalSelectedValues.filter(
+                        (selectedValues) => selectedValues !== option.value
+                      );
+                    } else {
+                      // If the option is not checked, we add it to the selected values
+                      newSelectedValue = [
+                        ...internalSelectedValues,
+                        option.value,
+                      ];
+                    }
+                    setInternalSelectedValues(newSelectedValue);
+                    onChange(newSelectedValue);
+                  }}
+                />
+                {option.children && renderOptions(option.children, depth + 1)}
+              </CheckboxGroupContainer>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  };
+  return <div>{renderOptions(options)}</div>;
+};
+export { CheckboxSize, CheckboxState };
