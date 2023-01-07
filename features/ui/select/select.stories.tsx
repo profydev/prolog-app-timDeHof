b4b8a7d14b0a193d0b4d019865f08c6eeb7e38e3
timDeHof <ttdehof@gmail.com>
2023-01-07 15:54:21 -0500

    UI kit select (#26)
    * started creating a select component
    
    * rewrote the select component for easy styling
    
    * added select option function to component
    
    * prettier problem
    
    * prettier problem
    
    * move open state enum up to the top of the list.

--- features/ui/select/select.stories.tsx
+++ features/ui/select/select.stories.tsx
@@ -1,30 +1,83 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { Select, SelectProps } from "./select";
+import { Select, SelectProps, SelectionStates } from "./select";
 import { action } from "@storybook/addon-actions";
 
 export default {
   component: Select,
   title: "UI/Select",
   parameters: {
-    componentSubtitle:
-      "The Select component is used as a dynamic form element. It displays a number of options that are exposed to the user when they click on the component. At this moment we don’t use multi-select elements.",
+    componentSubtitle: `The Select component is used as a dynamic form element.
+       It displays a number of options that are exposed to the user when they click on the component.
+       At this moment we don’t use multi-select elements.`,
+    actions: {
+      handles: ["click", ".radio"],
+    },
   },
 } as ComponentMeta<typeof Select>;
 
 const Template: ComponentStory<typeof Select> = (args: SelectProps) => (
-  <Select {...args} />
+  <div>
+    <Select {...args} onChange={action("onChange")} />
+  </div>
 );
 
-const options = ["Option 1", "Option 2", "Option 3"];
+const options = [
+  "Olivia Rhye",
+  "Phoenix Baker",
+  "Lana Steiner",
+  "Demi Wilkinson",
+  "Candice Wu",
+  "Natali Craig",
+  "Drew Cano",
+];
 
-export const Default = Template.bind({});
-Default.args = {
+export const Empty = Template.bind({});
+Empty.args = {
   options: options,
-  onChange: action("onChange"),
-  label: "Select",
-  value: options[0],
-  isFocused: false,
-  isDisabled: false,
-  isOpen: true,
+  label: "Team member",
+  defaultValue: "Select Team Member",
+  state: SelectionStates.Empty,
+  hintText: "This is a hint text to help user.",
+  selectedOption: "Select Team Member",
+};
+
+export const Filled = Template.bind({});
+Filled.args = {
+  options: options,
+  label: "Team member",
+  defaultValue: "Select Team Member",
+  state: SelectionStates.Filled,
+  hintText: "This is a hint text to help user.",
+  selectedOption: "Candice Wu",
+};
+
+export const Focused = Template.bind({});
+Focused.args = {
+  options: options,
+  label: "Team member",
+  defaultValue: "Select Team Member",
+  state: SelectionStates.Focused,
+  hintText: "This is a hint text to help user.",
+  selectedOption: "Candice Wu",
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  options: options,
+  label: "Team member",
+  defaultValue: "Select Team Member",
+  state: SelectionStates.Disabled,
+  hintText: "This is a hint text to help user.",
+  selectedOption: "Candice Wu",
+};
+
+export const Opened = Template.bind({});
+Opened.args = {
+  options: options,
+  label: "Team member",
+  defaultValue: "Select Team Member",
+  state: SelectionStates.Open,
+  hintText: "This is a hint text to help user.",
+  selectedOption: "Candice Wu",
 };
