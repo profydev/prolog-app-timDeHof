8a22b80a7d06a33131e92b60d2f128feb8a02f42
timDeHof <this.is.tim.dehof@gmail.com>
2023-03-29 20:14:42 -0400

    Update HeroSection component to add support for BackgroundTheme prop and new styling rules

--- features/ui/hero-section/herosection.tsx
+++ features/ui/hero-section/herosection.tsx
@@ -1,4 +1,64 @@
 import React from "react";
-export const HeroSection = () => {
-  return <div>Hero</div>;
+import Image from "next/image";
+import styled from "styled-components";
+import { color, breakpoint } from "@styles/theme";
+import { SectionHero, BackgroundTheme } from "@typings/landingPages.types";
+
+const Hero = styled.section<{ bgTheme: BackgroundTheme }>`
+  background: ${({ bgTheme }) => {
+    switch (bgTheme) {
+      case "light-gray":
+        return color("gray", 50);
+      default:
+        return "#fff";
+    }
+  }};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  & h1 {
+    color: ${color("gray", 900)};
+    font-size: 3.75rem;
+    font-family: "Inter";
+    font-weight: 600;
+    font-style: normal;
+    line-height: 4.5rem;
+    text-align: center;
+    letter-spacing: -0.02em;
+  }
+  & p {
+    color: ${color("gray", 500)};
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 1.25rem;
+    line-height: 1.875rem;
+    text-align: center;
+  }
+  & img {
+    width: 100%;
+    height: auto;
+  }
+
+  @media (min-width: ${breakpoint("desktop")}) {
+    & p {
+      white-space: pre;
+    }
+  }
+`;
+
+export const HeroSection = ({ data }: { data: SectionHero }) => {
+  const { title, subtitle, image, theme } = data;
+  return (
+    <Hero bgTheme={theme}>
+      <h1>{title}</h1>
+      <p>{subtitle}</p>
+      <Image
+        src={`https://prolog-api.profy.dev${image.src}`}
+        width={image.width}
+        height={image.height}
+        alt="hero image"
+      />
+    </Hero>
+  );
 };
---
+++ pages/index.tsx
@@ -0,0 +1,69 @@
+import Head from "next/head";
+import styled from "styled-components";
+import { Header } from "@features/ui/header";
+import { HeroSection } from "@features/ui/hero-section";
+import {
+  SectionHero,
+  SectionSocialProof,
+  SectionTestimonials,
+  SectionTypes,
+  TLandingPage,
+} from "@typings/landingPages.types";
+const ContactButton = styled.button`
+  position: absolute;
+  bottom: 2.5rem;
+  right: 2.5rem;
+  padding: 1rem;
+  background: #7f56d9;
+  border-radius: 50%;
+  box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    background: #6941c6;
+  }
+`;
+const PageWrapper = styled.div``;
+const Main = styled.main``;
+
+const IssuesPage = ({ data }: { data: TLandingPage }) => {
+  const hero = data.sections.find(
+    (section) => section.sectionType === SectionTypes.hero
+  ) as SectionHero;
+
+  return (
+    <PageWrapper>
+      <Head>
+        <title>{data.meta.title}</title>
+        <meta name="description" content={data.meta.description} />
+        <meta property="og:title" content={data.meta.title} />
+        <meta property="og:description" content={data.meta.description} />
+        <meta
+          property="og:image"
+          content={`https://prolog-api.profy.dev/${data.meta.image}`}
+        />
+      </Head>
+      <Header />
+      <Main>{hero && <HeroSection data={hero} />}</Main>
+      <ContactButton
+        onClick={() =>
+          alert(
+            "Implement this in Challenge 2 - Modal:\n\nhttps://profy.dev/rjs-challenge-modal"
+          )
+        }
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="/icons/message.svg" alt="Contact" />
+      </ContactButton>
+    </PageWrapper>
+  );
+};
+
+export default IssuesPage;
+
+export async function getServerSideProps() {
+  const res = await fetch(`https://prolog-api.profy.dev/content-page/home`);
+  const data = await res.json();
+  return { props: { data } };
+}
